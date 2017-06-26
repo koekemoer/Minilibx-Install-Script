4f1af2ad6ade243daa04ef3bf3f2e64733c863aa
cdebruyn <debruyn.sarel@gmail.com>
2017-06-26 21:38:35 +0200

    f' '

--- minilibx_linux.sh
+++ minilibx_linux.sh
@@ -7,7 +7,7 @@ if [ ! -f "/usr/include/X11/extensions/XShm.h" ]; then
 	sudo apt-get install libxext-dev --fix-missing
 fi
 
-#git clone https://github.com/abouvier/minilibx.git
+git clone https://github.com/abouvier/minilibx.git
 
 sudo make -C ./minilibx/ DESTDIR=/usr/local install
 
