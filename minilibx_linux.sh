15829bec8b0c2691963e0d0a7abc3a50e341199a
cdebruyn <debruyn.sarel@gmail.com>
2017-06-26 19:04:29 +0200

    initial commit

---
+++ minilibx_linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# First check that the dependencies are installed
+echo "Busy checking for dependencies"
+
+if [ ! -f "/usr/include/X11/extensions/XShm.h" ]; then
+	sudo apt-get install libxext-dev --fix-missing
+fi
+
+git clone https://github.com/abouvier/minilibx.git
+
+sudo make DESTDIR=/usr/local install
+
+echo "Successfully install Minilibx"
